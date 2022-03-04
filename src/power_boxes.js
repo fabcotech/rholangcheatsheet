0f87c193539d946233b30042d2bf2f9179cc8478
Prathyushmnchla <98600046+Prathyushmnchla@users.noreply.github.com>
2022-03-04 16:22:13 +0530

    Create power_boxes.js

---
+++ src/power_boxes.js
@@ -0,0 +1,11 @@
+const a = {
+  bgColor: "131,255,222",
+  classes: "text-black",
+  title: "Power Boxes",
+  lines: [
+    ["<pre>stdout(`rho:io:stdout`)</pre>", "stdout!( any_process ) \n prints to screen" ],
+    ["<pre>sha256Hash(`rho:crypto:sha256Hash`)</pre>", " sha256Hash!( byte_array, *return) \n calculates sha256hash "],
+  ]
+}
+
+export default a;
