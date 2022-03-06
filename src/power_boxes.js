cdc3aa2614cb969967111fa1b55e5eeb2b04ed4a
Prathyushmnchla <98600046+Prathyushmnchla@users.noreply.github.com>
2022-03-06 18:00:35 +0530

    Update power_boxes.js

--- src/power_boxes.js
+++ src/power_boxes.js
@@ -3,8 +3,11 @@ const a = {
   classes: "text-black",
   title: "Power Boxes",
   lines: [
-    ["<pre>stdout(`rho:io:stdout`)</pre>", "stdout!( any_process ) \n prints to screen" ],
-    ["<pre>sha256Hash(`rho:crypto:sha256Hash`)</pre>", " sha256Hash!( byte_array, *return) \n calculates sha256hash "],
+    ["<pre>stdout(`rho:io:stdout`)</pre>", "stdout!( any_process)  prints to screen" ],
+    ["<pre>sha256Hash(`rho:crypto:sha256Hash`)</pre>", "sha256Hash!( data_byte_array, return)"],
+    ["<pre>keccak256Hash(`rho:crypto:keccak256Hash`)</pre>", "keccak256Hash!( data_byte_array, return)"],
+    ["<pre>secp256k1Verify(`rho:crypto:secp256k1Verify`)</pre>", "secp256k1Verify!(data_byte_array, signature, public_key, return)"],
+    ["<pre>ed25519Verify(`rho:crypto:ed25519Verify`)</pre>", "ed25519Verify!(data_byte_array, signature, public_key, return)"],
   ]
 }
 
